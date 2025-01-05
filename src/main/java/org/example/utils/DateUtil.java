57b7c9c36e0e8cf57bbe6d4d2bda92fd2b506ae2
egorkudd <egor.ku.03@gmail.com>
2025-01-05 13:06:38 +0300

    fix: hot fix

--- src/main/java/org/example/utils/DateUtil.java
+++ src/main/java/org/example/utils/DateUtil.java
@@ -8,14 +8,9 @@ import java.util.Date;
 
 public class DateUtil {
     public static int age(Date birthdayDate) {
-//        Calendar today = Calendar.getInstance();
-//        Calendar birthday = Calendar.getInstance();
-//        birthday.setTime(birthdayDate);
-
         return Period.between(
                 birthdayDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
                 LocalDate.now()
         ).getYears();
-//        return today.get(Calendar.YEAR) - birthday.get(Calendar.YEAR);
     }
 }
