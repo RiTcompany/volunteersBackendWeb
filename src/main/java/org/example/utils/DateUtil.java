241d0f472d3ad600fa220949152bfc183b3c8153
egorkudd <egor.ku.03@gmail.com>
2025-01-05 13:06:07 +0300

    fix: hot DateUtil age method

--- src/main/java/org/example/utils/DateUtil.java
+++ src/main/java/org/example/utils/DateUtil.java
@@ -1,14 +1,21 @@
 package org.example.utils;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtil {
     public static int age(Date birthdayDate) {
-        Calendar today = Calendar.getInstance();
-        Calendar birthday = Calendar.getInstance();
-        birthday.setTime(birthdayDate);
+//        Calendar today = Calendar.getInstance();
+//        Calendar birthday = Calendar.getInstance();
+//        birthday.setTime(birthdayDate);
 
-        return today.get(Calendar.YEAR) - birthday.get(Calendar.YEAR);
+        return Period.between(
+                birthdayDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
+                LocalDate.now()
+        ).getYears();
+//        return today.get(Calendar.YEAR) - birthday.get(Calendar.YEAR);
     }
 }
